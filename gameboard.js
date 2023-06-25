b16cf921e74dfe4f2cc0f8ec24f0c42308663187
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-06-25 14:54:18 +0200

    Add receiveAttack, allShipsSunk methods for boards

--- gameboard.js
+++ gameboard.js
@@ -1,6 +1,13 @@
 import ShipFactory from './ship';
 
 const Gameboard = () => {
+  /**
+   * Keep an array of all ships. Sunk ships remain on the array.
+   * This allows for easier and quicker access to ship data than by going over
+   * the board array.
+   */
+  const ships = [];
+
   const locationProperties = {
     shipPresent: false,
     shipID: undefined,
@@ -47,13 +54,39 @@ const Gameboard = () => {
     // TODO: check if there is no ship adjacent
 
     const ship = ShipFactory(name, length);
+
+    // Add a reference to the ship to the board in each location it occupies
     array.forEach(([row, column]) => {
       board[row][column].shipPresent = true;
       board[row][column].shipID = ship;
     });
+
+    ships.push(ship);
   };
 
-  return { board, placeShip };
+  const receiveAttack = (row, column) => {
+    const attackedLocation = board[row][column];
+
+    // Ignore multiple attacks on same location
+    if (attackedLocation.wasAttacked) return;
+
+    attackedLocation.wasAttacked = true;
+
+    if (attackedLocation.shipPresent) {
+      attackedLocation.shipID.hit();
+    } else {
+      attackedLocation.attackMissed = true;
+    }
+  };
+
+  const allShipsSunk = () => {
+    if (ships.length === 0) return false;
+    return ships.every((ship) => ship.isSunk());
+  };
+
+  const getBoard = () => board;
+
+  return { getBoard, placeShip, receiveAttack, allShipsSunk };
 };
 
 export default Gameboard;
--- gameboard.test.js
+++ gameboard.test.js
@@ -1,35 +1,92 @@
 import Gameboard from './gameboard';
 
-test(`Expect a ship with shipID 'destroyer' in the top left corner, horizontally, with length 5`, () => {
-  const board1 = Gameboard();
-  board1.placeShip(0, 0, 'destroyer', 5, 'horizontal');
-  expect(board1.board[0][0]).toHaveProperty('shipPresent', true);
-  expect(board1.board[0][1]).toHaveProperty('shipPresent', true);
-  expect(board1.board[0][4]).toHaveProperty('shipPresent', true);
-  expect(board1.board[0][4].shipID.getName()).toBe('destroyer');
+describe('Placing ships', () => {
+  test(`Expect a ship with shipID 'destroyer' in the top left corner, horizontally, with length 5`, () => {
+    const board1 = Gameboard();
+    board1.placeShip(0, 0, 'destroyer', 5, 'horizontal');
+    expect(board1.getBoard()[0][0]).toHaveProperty('shipPresent', true);
+    expect(board1.getBoard()[0][1]).toHaveProperty('shipPresent', true);
+    expect(board1.getBoard()[0][4]).toHaveProperty('shipPresent', true);
+    expect(board1.getBoard()[0][4].shipID.getName()).toBe('destroyer');
 
-  expect(board1.board[0][7]).toHaveProperty('shipPresent', false);
-  expect(board1.board[0][9]).toHaveProperty('shipPresent', false);
-  expect(board1.board[2][7]).toHaveProperty('shipPresent', false);
-  expect(board1.board[1][0]).toHaveProperty('shipPresent', false);
+    expect(board1.getBoard()[0][7]).toHaveProperty('shipPresent', false);
+    expect(board1.getBoard()[0][9]).toHaveProperty('shipPresent', false);
+    expect(board1.getBoard()[2][7]).toHaveProperty('shipPresent', false);
+    expect(board1.getBoard()[1][0]).toHaveProperty('shipPresent', false);
+  });
+
+  test(`Expect a ship with shipID 'battleship' in the third row, fourth column, vertically, with length 4`, () => {
+    const board1 = Gameboard();
+    board1.placeShip(2, 3, 'battleship', 4, 'vertical');
+    expect(board1.getBoard()[0][0]).toHaveProperty('shipPresent', false);
+    expect(board1.getBoard()[2][3]).toHaveProperty('shipPresent', true);
+    expect(board1.getBoard()[3][3]).toHaveProperty('shipPresent', true);
+    expect(board1.getBoard()[4][3]).toHaveProperty('shipPresent', true);
+    expect(board1.getBoard()[5][3]).toHaveProperty('shipPresent', true);
+    expect(board1.getBoard()[5][3].shipID.getName()).toBe('battleship');
+    expect(board1.getBoard()[6][3]).toHaveProperty('shipPresent', false);
+    expect(board1.getBoard()[7][3]).toHaveProperty('shipPresent', false);
+    expect(board1.getBoard()[7][3]).toHaveProperty('shipPresent', false);
+  });
+
+  test('Same ship instance is referred to across locations', () => {
+    const board1 = Gameboard();
+    board1.placeShip(2, 3, 'battleship', 4, 'vertical');
+    expect(board1.getBoard()[2][3].shipID).toBe(board1.getBoard()[3][3].shipID);
+  });
 });
 
-test(`Expect a ship with shipID 'battleship' in the third row, fourth column, vertically, with length 4`, () => {
-  const board1 = Gameboard();
-  board1.placeShip(2, 3, 'battleship', 4, 'vertical');
-  expect(board1.board[0][0]).toHaveProperty('shipPresent', false);
-  expect(board1.board[2][3]).toHaveProperty('shipPresent', true);
-  expect(board1.board[3][3]).toHaveProperty('shipPresent', true);
-  expect(board1.board[4][3]).toHaveProperty('shipPresent', true);
-  expect(board1.board[5][3]).toHaveProperty('shipPresent', true);
-  expect(board1.board[5][3].shipID.getName()).toBe('battleship');
-  expect(board1.board[6][3]).toHaveProperty('shipPresent', false);
-  expect(board1.board[7][3]).toHaveProperty('shipPresent', false);
-  expect(board1.board[7][3]).toHaveProperty('shipPresent', false);
+describe('Attacking', () => {
+  test('Ship registers hit', () => {
+    const board1 = Gameboard();
+    board1.placeShip(0, 0, 'poor test ship', 1, 'horizontal');
+    expect(board1.getBoard()[0][0].shipID.isSunk()).toBe(false);
+    board1.receiveAttack(0, 0);
+    expect(board1.getBoard()[0][0].shipID.isSunk()).toBe(true);
+  });
+  test('Ship is sunk (only) after neccessary number of hits', () => {
+    const board1 = Gameboard();
+    board1.placeShip(0, 0, 'poor test ship', 2, 'horizontal');
+    expect(board1.getBoard()[0][0].shipID.isSunk()).toBe(false);
+    board1.receiveAttack(0, 0);
+    expect(board1.getBoard()[0][0].shipID.isSunk()).toBe(false);
+    board1.receiveAttack(0, 1);
+    expect(board1.getBoard()[0][0].shipID.isSunk()).toBe(true);
+  });
+  test('Attacking a ship in the same location twice cannot sink it', () => {
+    const board1 = Gameboard();
+    board1.placeShip(0, 0, 'poor test ship', 2, 'horizontal');
+    expect(board1.getBoard()[0][0].shipID.isSunk()).toBe(false);
+    board1.receiveAttack(0, 0);
+    expect(board1.getBoard()[0][0].shipID.isSunk()).toBe(false);
+    board1.receiveAttack(0, 0);
+    expect(board1.getBoard()[0][0].shipID.isSunk()).toBe(false);
+    board1.receiveAttack(0, 1);
+    expect(board1.getBoard()[0][0].shipID.isSunk()).toBe(true);
+  });
+  test('Missed attacks are properly registered on the board', () => {
+    const board1 = Gameboard();
+    board1.placeShip(0, 0, 'poor test ship', 2, 'horizontal');
+    expect(board1.getBoard()[1][1].wasAttacked).toBe(false);
+    board1.receiveAttack(1, 1);
+    expect(board1.getBoard()[1][1].wasAttacked).toBe(true);
+    board1.receiveAttack(1, 1);
+    expect(board1.getBoard()[1][1].wasAttacked).toBe(true);
+  });
 });
 
-test('Same ship instance is referred to across locations', () => {
-  const board1 = Gameboard();
-  board1.placeShip(2, 3, 'battleship', 4, 'vertical');
-  expect(board1.board[2][3].shipID).toBe(board1.board[3][3].shipID);
+describe('Board state', () => {
+  test('Report all ships sunk', () => {
+    const board1 = Gameboard();
+    expect(board1.allShipsSunk()).toBe(false);
+    board1.placeShip(0, 0, 'poor ship', 1, 'vertical');
+    board1.placeShip(2, 2, 'poor ship', 2, 'vertical');
+    expect(board1.allShipsSunk()).toBe(false);
+    board1.receiveAttack(0, 0);
+    expect(board1.allShipsSunk()).toBe(false);
+    board1.receiveAttack(2, 2);
+    expect(board1.allShipsSunk()).toBe(false);
+    board1.receiveAttack(3, 2);
+    expect(board1.allShipsSunk()).toBe(true);
+  });
 });
