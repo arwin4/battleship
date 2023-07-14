8b0f2e7dff093eb92fe06eee4e2976123bca1ed8
Arwin Yella <16850195+arwin4@users.noreply.github.com>
2023-07-14 11:32:59 +0200

    Fix ship test by correcting factory func arguments

--- ship.test.js
+++ ship.test.js
@@ -1,22 +1,22 @@
 import ShipFactory from './ship';
 
 test('Ship with different lengths are only sunk after the expected amount of hits', () => {
-  const ship1 = ShipFactory(1);
+  const ship1 = ShipFactory('ship1', 1);
   expect(ship1.isSunk()).toBe(false);
 
-  const ship2 = ShipFactory(1);
+  const ship2 = ShipFactory('ship2', 1);
   expect(ship2.isSunk()).toBe(false);
   ship2.hit(); // Should sink here
   expect(ship2.isSunk()).toBe(true);
 
-  const ship3 = ShipFactory(2);
+  const ship3 = ShipFactory('ship3', 2);
   expect(ship3.isSunk()).toBe(false);
   ship3.hit();
   expect(ship3.isSunk()).toBe(false);
   ship3.hit(); // Should sink here
   expect(ship3.isSunk()).toBe(true);
 
-  const ship4 = ShipFactory(3);
+  const ship4 = ShipFactory('ship4', 3);
   expect(ship4.isSunk()).toBe(false);
   ship4.hit();
   expect(ship4.isSunk()).toBe(false);
@@ -27,7 +27,7 @@ test('Ship with different lengths are only sunk after the expected amount of hit
 });
 
 test('Ship continues to be sunk even it continues to get hit', () => {
-  const ship3 = ShipFactory(2);
+  const ship3 = ShipFactory('ship3', 2);
   expect(ship3.isSunk()).toBe(false);
   ship3.hit();
   expect(ship3.isSunk()).toBe(false);
